04a7e3a235f863cf5e93c0e37d7372ee2d7841bd
DiegoAGtz <diegogtz1207@gmail.com>
2021-10-29 23:46:58 -0500

    Código inicial, recepción Señal

--- MM_Proyecto_Final.X/main.c
+++ MM_Proyecto_Final.X/main.c
@@ -14,29 +14,35 @@
 
 #define _XTAL_FREQ 4000000      // Frecuencia del micro
 
-void Configuracion(void);
+char Bandera = 0;
+int Valor = 0;
+
+void configuracion(void);
+void __interrupt(high_priority) IAP(void);
 
 void main(void) {
-    char A = 65;
-    Configuracion();
+    configuracion();
     __delay_ms(10); // Tiempo de asentamiento para el módulo    
 
     while (1) {
-        PORTA = A;
-        TXREG1 = A;
+        LATAbits.LA7 ^= 1;
         __delay_ms(500);
-        A += 1;
-        if (A > 90) A = 65;
     }
 
     return;
 }
 
-void Configuracion(void) {
+void configuracion(void) {
     TRISA = 0;
+    TRISB = 0;
     TRISC = 0xC0; // b?11000000?
     ANSELA = 0;
+    ANSELB = 0;
     ANSELC = 0;
+
+    INTCON = 0b10000000; // Activa interrupciones de alta prioridad
+    RCONbits.IPEN = 1; // Activa alta y baja prioridad
+    PIE1bits.RCIE = 1;
 
     OSCCON = 0x53; //4MHz
 
@@ -44,4 +50,36 @@ void Configuracion(void) {
     TXSTA1 = 0x24;
     SPBRGH1 = 0x00;
     RCSTA1 = 0x90;
+
+    CCP1CON = 0x0F;
+    T2CON = 0x07;
+    PR2 = 155;
+    CCPR1L = 0;
+}
+
+void __interrupt(high_priority) IAP(void) {
+    char Letra;
+    Letra = RCREG1;
+    if(Letra == 65) {
+        LATAbits.LA0 ^= 1;
+    } else if(Letra == 66) {
+        LATAbits.LA1 ^= 1;
+    } else if(Letra == 67) {
+        if(Bandera == 0) {
+            Valor += 30;
+            CCPR1L = Valor;
+            LATB = Valor;
+            if(Valor > 149) {
+                Bandera = 1;
+            }
+        } else {
+            Valor -= 30;
+            CCPR1L = Valor;
+            LATB = Valor;
+            if(Valor < 30) {
+                Bandera = 0;
+            }
+        }
+    }
+    // PIR1bits.RCIF = 0;   // No es necesaria. Se limpia al leer RCREG1
 }
