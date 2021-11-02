8be52e42f1fd96d5e71cd0efde30d91b9a474f4b
DiegoAGtz <diegogtz1207@gmail.com>
2021-11-02 15:47:41 -0600

    Implementación de la pantalla LCD, lectura y conversión de cadenas, mapeo de datos.

--- MM_Proyecto_Final.X/main.c
+++ MM_Proyecto_Final.X/main.c
@@ -11,19 +11,31 @@
 #pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port Enable (ICPORT disabled)
 
 #include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define _XTAL_FREQ 4000000      // Frecuencia del micro
 
 char Bandera = 0;
+char Indice = 0;
 int Valor = 0;
 
 void configuracion(void);
 void __interrupt(high_priority) IAP(void);
+void InicializaLCD(void);
+void putch(char data);
+void putcm(char data);
+void limpiaLCD(void);
+int mapeo(int valor, int minEntrada, int maxEntrada, int minSalida, int maxSalida);
 
 void main(void) {
     configuracion();
+    InicializaLCD();
     __delay_ms(10); // Tiempo de asentamiento para el módulo    
 
+    printf(" Curso de M&M ");
+    putcm(0xC2);
+    printf(" Year: %d ", 2021);
     while (1) {
         LATAbits.LA7 ^= 1;
         __delay_ms(500);
@@ -36,9 +48,11 @@ void configuracion(void) {
     TRISA = 0;
     TRISB = 0;
     TRISC = 0xC0; // b?11000000?
+    TRISD = 0;
     ANSELA = 0;
     ANSELB = 0;
     ANSELC = 0;
+    ANSELD = 0;
 
     INTCON = 0b10000000; // Activa interrupciones de alta prioridad
     RCONbits.IPEN = 1; // Activa alta y baja prioridad
@@ -58,28 +72,86 @@ void configuracion(void) {
 }
 
 void __interrupt(high_priority) IAP(void) {
-    char Letra;
-    Letra = RCREG1;
-    if(Letra == 65) {
-        LATAbits.LA0 ^= 1;
-    } else if(Letra == 66) {
-        LATAbits.LA1 ^= 1;
-    } else if(Letra == 67) {
-        if(Bandera == 0) {
-            Valor += 30;
-            CCPR1L = Valor;
-            LATB = Valor;
-            if(Valor > 149) {
-                Bandera = 1;
-            }
-        } else {
-            Valor -= 30;
-            CCPR1L = Valor;
-            LATB = Valor;
-            if(Valor < 30) {
-                Bandera = 0;
-            }
-        }
+    char Letra[3];
+    Letra[Indice] = RCREG1;
+    if (Letra[Indice] == '\r' || Indice == 2) {
+        Indice = 0;
+        LATB = atoi(Letra);
+        Valor = mapeo(atoi(Letra), 0, 100, 0, 149);
+        CCPR1L = Valor;
+        limpiaLCD();
+        putcm(0xC2);
+        printf("%d%% - %d", atoi(Letra), Valor);
+    } else if (Letra[Indice] < 48 || Letra[Indice] > 57) {
+        // Código incorrecto
+        limpiaLCD();
+        putcm(0xC2);
+        printf("Error.");
+        Indice = 0;
+        LATB = 255;
+    } else {
+        Indice++;
     }
     // PIR1bits.RCIF = 0;   // No es necesaria. Se limpia al leer RCREG1
+}
+
+void InicializaLCD(void) {
+    __delay_ms(30);
+    putcm(0x02); // Inicializa en modo 4 bits
+    __delay_ms(1);
+
+    putcm(0x28); // Inicializa en 2 lineas 5x7
+    __delay_ms(1);
+
+    putcm(0x2C);
+    __delay_ms(1);
+
+    putcm(0x0C);
+    __delay_ms(1);
+
+    putcm(0x06);
+    __delay_ms(1);
+
+    putcm(0x80); // Posiciona el cursor en 1,1
+    __delay_ms(1);
+}
+
+void putch(char data) {
+    char Activa;
+    Activa = data & 0xF0;
+    LATD = Activa | 0x05; // 0bxxxx0101
+    __delay_us(10);
+
+    LATD = Activa | 0x01; // 0bxxxx0001
+    __delay_ms(1);
+
+    Activa = data << 4;
+    LATD = Activa | 0x05;
+    __delay_us(10);
+    LATD = Activa | 0x01;
+}
+
+void putcm(char data) {
+    char Activa;
+    Activa = data & 0xF0;
+    LATD = Activa | 0x04;
+    __delay_us(10);
+
+    LATD = Activa;
+    __delay_ms(1);
+    Activa = data << 4;
+
+    LATD = Activa | 0x04;
+    __delay_us(10);
+    LATD = Activa;
+}
+
+void limpiaLCD(void) {
+    putcm(0x01); //Limpiamos LCD
+    putcm(0x84); //Ponemos el cursor en la posici?n inicial 0,0 del LCD
+    printf("Proyecto Final");
+}
+
+int mapeo(int valor, int minEntrada, int maxEntrada, int minSalida, int maxSalida) {
+    return (int) ((valor - minEntrada)*(maxSalida - minSalida) / (maxEntrada - minEntrada) + minSalida);
 }
